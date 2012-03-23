cf97b5f06e54144df7750b213aa8c67be9eaba60
Kristoffer Berdal <web@flexd.net>
2012-03-23 13:25:49 +0100

    initial netbeans project

---
+++ src/prosjekt/Prosjekt.java
@@ -0,0 +1,18 @@
+/*
+ */
+package prosjekt;
+
+/**
+ *
+ * @author Kristoffer Berdal <web@flexd.net>
+ */
+public class Prosjekt {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+    System.out.println("Kittens!");
+  }
+}
