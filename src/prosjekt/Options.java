e55e6d2458b877a099e59802e9d837bbd3c579db
Kristoffer Berdal <web@flexd.net>
2012-04-23 11:00:15 +0200

    Options WIP

---
+++ src/prosjekt/Options.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prosjekt;
+
+import java.awt.Color;
+import java.util.HashMap;
+
+/**
+ *
+ * @author kristoffer
+ */
+public class Options {
+  public static Color uiMainColor = Color.LIGHT_GRAY;
+}
